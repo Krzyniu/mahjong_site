1f79274ce2096c0d982f51899e12b06123a066f5
Bartosz Gałecki <bg@ds.akquinet.pl>
2020-01-27 12:50:07 +0100

    Nowy mecz

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -8,5 +8,22 @@ class MatchesController < ApplicationController
     end
      
     def new
+        @match = Match.new
     end
+
+    def create
+        @match = Match.new(match_params)
+ 
+        if @match.save
+            redirect_to matches_path
+        else
+            render 'new'
+        end
+        
+    end
+
+    private
+        def match_params
+            params.require(:match).permit(:player_1_id, :player_2_id, :player_3_id, :player_4_id, :player_1_score, :player_2_score, :player_3_score, :player_4_score)
+        end
 end
