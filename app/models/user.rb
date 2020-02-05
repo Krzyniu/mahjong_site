8af8ff861d39a1363fbcc2dc1730a7ff847e119e
krzyniu <gohanssj101@wp.pl>
2020-02-05 16:27:28 +0100

    User icon in navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   include Clearance::User
   has_many :matches, :class_name => 'Match', :foreign_key => 'match_id'
 
-  has_attached_file :image, styles: {large: "640x640>", medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :image, styles: {large: "640x640>", medium: "300x300>", thumb: "100x100>", icon: "50x50" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
